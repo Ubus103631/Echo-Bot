5517890cda06f89515909e5c3006cffc1f0dd37f
Ubus103631 <66058631+Ubus103631@users.noreply.github.com>
2025-06-13 11:36:21 +0800

    Update app.py
    去掉\n

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def callback():
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        reply_text = event.message.text + " (reply token: " + event.reply_token + ")" + \n + "USERID: " + event.source.user_id
+        reply_text = event.message.text + " (reply token: " + event.reply_token + ")" + "USERID: " + event.source.user_id
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
